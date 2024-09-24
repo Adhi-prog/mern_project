4a0e7066f9deb7a58f7df77729b04f334544199a
Adhi-prog <adithyaradhakrishanan2022@gmail.com>
2024-09-24 16:10:56 +0530

    updated

--- stack/server/config/db.js
+++ stack/server/config/db.js
@@ -1,16 +1,25 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGOURL ||
-    "mongodb+srv://adhi:adhi@sample.nqzmb.mongodb.net/?retryWrites=true&w=majority&appName=sample"
-);
+// mongoose.connect(
+//   process.env.MONGOURL ||
+//     "mongodb+srv://adhi:adhi@sample.nqzmb.mongodb.net/?retryWrites=true&w=majority&appName=sample"
+// );
+
+// const connection = mongoose.connection;
+
+// connection.on("connected", () => {
+//   console.log("Db connected");
+// });
+// connection.on("erroe", () => {
+//   console.log("Db error");
+// });
+// module.exports = mongoose;
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.MONGOURL)
 
 const connection = mongoose.connection;
+connection.on('connected', () => console.log("DB Connected"))
+connection.on('error', () => console.log("DB Error"))
 
-connection.on("connected", () => {
-  console.log("Db connected");
-});
-connection.on("erroe", () => {
-  console.log("Db error");
-});
-module.exports = mongoose;
+module.exports = mongoose
